4b9b27c4887e125dc5c96bd913de1d2c108b20d3
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-06-28 09:56:44 +0100

    Initalised the camera fragment

---
+++ app/src/main/java/com/codersguidebook/camera/ui/camera/CameraFragment.kt
@@ -0,0 +1,32 @@
+package com.codersguidebook.camera.ui.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codersguidebook.camera.databinding.FragmentCameraBinding
+
+class CameraFragment : Fragment() {
+
+    private var _binding: FragmentCameraBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
