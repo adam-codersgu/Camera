5e1e5fab8083902c3405047e50eebf1ca72ec996
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-06-28 09:45:07 +0100

    Handled user permissions

---
+++ app/src/main/java/com/codersguidebook/camera/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.codersguidebook.camera
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.codersguidebook.camera.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!CameraPermissionHelper.hasCameraPermission(this) || !CameraPermissionHelper.hasStoragePermission(this)) {
+            CameraPermissionHelper.requestPermissions(this)
+        } else recreate()
+    }
+
+    object CameraPermissionHelper {
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+        private const val READ_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
+
+        fun hasCameraPermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        }
+
+        fun hasStoragePermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, READ_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        }
+
+        fun requestPermissions(activity: Activity) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION)) {
+                AlertDialog.Builder(activity).apply {
+                    setMessage(activity.getString(R.string.permission_required))
+                    setPositiveButton(activity.getString(R.string.ok)) { _, _ ->
+                        ActivityCompat.requestPermissions(activity, arrayOf(CAMERA_PERMISSION, READ_PERMISSION), 1)
+                    }
+                    show()
+                }
+            } else {
+                ActivityCompat.requestPermissions(activity, arrayOf(CAMERA_PERMISSION, READ_PERMISSION), 1)
+            }
+        }
+    }
+}
