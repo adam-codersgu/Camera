4af4a295eeb822aa15c5129b5a016ce3a24aa6ca
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-06-29 22:21:04 +0100

    Added code for initialising the camera

--- app/src/main/java/com/codersguidebook/camera/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/codersguidebook/camera/ui/camera/CameraFragment.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.codersguidebook.camera.MainActivity
+import com.codersguidebook.camera.R
 import com.codersguidebook.camera.databinding.FragmentCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
 
@@ -14,6 +23,7 @@ class CameraFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +33,40 @@ class CameraFragment : Fragment() {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        openCamera()
+    }
+
+    private fun openCamera() {
+        if (MainActivity.CameraPermissionHelper.hasCameraPermission(requireActivity()) &&
+            MainActivity.CameraPermissionHelper.hasStoragePermission(requireActivity())) {
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
+
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(binding.cameraFeed.surfaceProvider)
+                    }
+
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                // TODO: Initialise the ImageCapture instance here
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                } catch (e: IllegalStateException) {
+                    Toast.makeText(requireActivity(), resources.getString(R.string.error_connecting_camera), Toast.LENGTH_LONG).show()
+                }
+            }, ContextCompat.getMainExecutor(requireActivity()))
+        } else MainActivity.CameraPermissionHelper.requestPermissions(requireActivity())
     }
 
     override fun onDestroyView() {
