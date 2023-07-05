3d2e0806b5f34523e9e879e3546bf72e6b8a9f21
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-07-05 23:49:32 +0100

    Created Photo data class

---
+++ app/src/main/java/com/codersguidebook/camera/Photo.kt
@@ -0,0 +1,9 @@
+package com.codersguidebook.camera
+
+import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Photo(val id: Long,
+                 val uri: Uri) : Parcelable
