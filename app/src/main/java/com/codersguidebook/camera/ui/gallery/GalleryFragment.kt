a5f93f0a5db11f3a32291e8ef55af1aa8613ad13
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-07-07 18:35:44 +0100

    Created gallery fragment

--- app/src/main/java/com/codersguidebook/camera/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codersguidebook/camera/ui/gallery/GalleryFragment.kt
@@ -4,35 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.codersguidebook.camera.databinding.FragmentHomeBinding
+import com.codersguidebook.camera.GalleryViewModel
+import com.codersguidebook.camera.MainActivity
+import com.codersguidebook.camera.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: GalleryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[GalleryViewModel::class.java]
+        return binding.root
+    }
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // TODO: Apply the adapter to the RecyclerView
+
+        viewModel.photos.observe(viewLifecycleOwner) { photos ->
+            photos?.let {
+                // TODO: Load the photo previews here
+            }
         }
-        return root
+
+        if (MainActivity.CameraPermissionHelper.hasStoragePermission(requireActivity())) viewModel.loadPhotos()
+        else MainActivity.CameraPermissionHelper.requestPermissions(requireActivity())
     }
 
     override fun onDestroyView() {
